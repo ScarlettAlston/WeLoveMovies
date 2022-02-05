0dd87e4ff14053cb6b753b2a813d4c07d4da3359
Scarlett Alston <scarlett938@gmail.com>
2022-02-05 14:18:53 -0800

    fixed file structure

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -2,6 +2,7 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./theaters.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
+//theaters list router
 router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
