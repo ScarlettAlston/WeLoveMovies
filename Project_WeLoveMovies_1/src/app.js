1948afc31fca5a1e7149fc689845e8ed78394ad4
Scarlett Alston <scarlett938@gmail.com>
2022-02-05 13:28:16 -0800

    finished app.js

--- Project_WeLoveMovies_1/src/app.js
+++ Project_WeLoveMovies_1/src/app.js
@@ -2,10 +2,22 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//variables that define router files
 const moviesRouter = require("./movies/movie.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
+
+app.use(express.json());
+
+//router paths
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
+
+//error paths
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
